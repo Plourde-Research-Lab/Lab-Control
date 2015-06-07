1c2b0b89b76da9af98c9e3378a06ca29e2b91b3c
Caleb Howington <howingtonc@gmail.com>
2015-06-07 14:10:29 -0400

    Changed to port 80

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 
 var app = express()
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 
 app.use(compress())
